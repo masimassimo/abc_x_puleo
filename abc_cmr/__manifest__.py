200ff1885fc874af4e3c31c8621cef4580c8a2e3
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-04 16:02:13 +0100

    Update __manifest__.py

--- abc_cmr/__manifest__.py
+++ abc_cmr/__manifest__.py
@@ -20,7 +20,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'data/cmr_paperformat.xml',
         'views/report_cmr.xml',
         'views/stock_picking_view.xml',

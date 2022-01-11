f9c4fe2d64187a62a53bd23900ba650e633a0237
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-11 16:49:22 +0100

    Update __manifest__.py

--- abc_campi_product_template/__manifest__.py
+++ abc_campi_product_template/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "ABC - abc_campi_product_template",
 
@@ -10,21 +9,15 @@
 
     'author': "A.B.C. srl",
     'website': "https://www.abcstrategie.it/",
-    
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+
     'category': 'Product',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base', "product"],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [],
 }

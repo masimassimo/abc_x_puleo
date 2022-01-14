5ad49394f9c70ff8899336b85ecc2824239ac605
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-14 09:35:01 +0100

    Update models.py

--- abc_campi_res_partner/models/models.py
+++ abc_campi_res_partner/models/models.py
@@ -12,12 +12,12 @@ class resPartner(models.Model):
     _name = "res.partner"
     _inherit = "res.partner"
     
-    cliente = fields.Boolean(string="Cliente", default = False, store=True)
-    fornitore = fields.Boolean(string="Fornitore", default = False, store=True)
-    banca = fields.Boolean(string="Banca", default = False, store=True)
-    vettore = fields.Boolean(string="Vettore", default = False, store=True)
-    agente = fields.Boolean(string="Agente", default = False, store=True)
-    dipendente = fields.Boolean(string="Dipendente", default = False, store=True)
+    #cliente = fields.Boolean(string="Cliente", default = False, store=True)
+    #fornitore = fields.Boolean(string="Fornitore", default = False, store=True)
+    #banca = fields.Boolean(string="Banca", default = False, store=True)
+    #vettore = fields.Boolean(string="Vettore", default = False, store=True)
+    #agente = fields.Boolean(string="Agente", default = False, store=True)
+    #dipendente = fields.Boolean(string="Dipendente", default = False, store=True)
     
     cliente_id_sam = fields.Char(string = "Cliente ID SAM", store = True)
     persona_id_sam = fields.Char(string = "Persona ID SAM", store = True)

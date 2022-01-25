e96ea78f3df33d4b895516921b9fa57551cc0316
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-01-25 16:28:49 +0100

    Update __manifest__.py

--- abc_dipendente_azienda/abc_dipendente_azienda/__manifest__.py
+++ abc_dipendente_azienda/abc_dipendente_azienda/__manifest__.py
@@ -17,7 +17,7 @@
     'website': "https://www.abcstrategie.it/",
 
     'category': 'Sale',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],

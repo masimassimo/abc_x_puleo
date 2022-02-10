74c67550b1d6fb526a52b198c68bee470e49857e
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-02-10 12:57:07 +0100

    Update models.py

--- abc_dipendente_azienda/abc_dipendente_azienda/models/models.py
+++ abc_dipendente_azienda/abc_dipendente_azienda/models/models.py
@@ -12,7 +12,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     nominativo_di_riferimento = fields.Many2one(
-        'res.partner', string='Nominativo di riferimento', readonly=True,
-        states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+        'res.partner', string='Nominativo di riferimento', readonly=False,
+        #states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
         change_default=True, index=True, tracking=1,
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id), ('parent_id', '=', partner_id)]",)

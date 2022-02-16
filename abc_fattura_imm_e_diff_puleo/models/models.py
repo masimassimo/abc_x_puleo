17bf8c04256317a7ff2e88ef5b1979bd5b64b06b
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-02-16 13:24:37 +0100

    Update models.py

--- abc_fattura_imm_e_diff_puleo/models/models.py
+++ abc_fattura_imm_e_diff_puleo/models/models.py
@@ -12,14 +12,9 @@ class accountMove(models.Model):
     _name = "account.move"
     _inherit = "account.move"
     
-    tipo_container = fields.Char(string = "Tipo Container")
-    n_container = fields.Char(string = "N° Container")
-    n_sigillo = fields.Char(string = "N° sigillo")
     #agente = fields.Many2one('res.partner', string= "Addetto vendite", related="invoice_line_ids.sale_line_ids.order_id.user_id", store = True, readonly = False)
     
 #Eredito il modulo sale.order per accedervi.
 class SaleOrder(models.Model):
     _name = "sale.order"
     _inherit = "sale.order"
-    
-

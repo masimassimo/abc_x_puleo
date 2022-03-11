7fa26b4ae644ebfdb1b1a2bc3f4d648996e4ef4a
ABC-Strategie-GitHub <abc.strategie.github@gmail.com>
2022-03-11 09:34:16 +0100

    [UPD] Aggiunto report Fattura PRO FORMA. Aggiornato il manifest.

--- abc_preventivo_e_ordine_puleo/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ abc_preventivo_e_ordine_puleo/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -2,15 +2,15 @@
 {
     'name': "ABC - abc_preventivo_e_ordine_puleo",
 
-    'summary': """Modulo di stampa personalizzato Puleo per Preventivo / Ordine di vendita e di acquisto.""",
+    'summary': """Modulo di stampa personalizzato Puleo per Preventivo / Ordine di vendita e di acquisto. Fattura PRO FORMA inclusa.""",
 
-    'description': """Modulo di stampa personalizzato Puleo per Preventivo / Ordine di vendita e di acquisto.""",
+    'description': """Modulo di stampa personalizzato Puleo per Preventivo / Ordine di vendita e di acquisto. Fattura PRO FORMA inclusa.""",
 
     'author': "A.B.C. Srl",
     'website': "https://www.abcstrategie.it/",
 
     'category': 'Sale',
-    'version': '0.1',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'purchase', 'purchase_order_secondary_unit', 'product_secondary_unit'],
@@ -23,7 +23,8 @@
         'report/preventivo_e_ordine_puleo.xml',
         'report/preventivo_acquisto_puleo.xml',
         'report/acquisto_print_report.xml',
-        'report/ordine_acquisto_puleo.xml'
+        'report/ordine_acquisto_puleo.xml',
+        'report/fattura_pro_forma_puleo.xml',
 
     ],
     "installable":True,

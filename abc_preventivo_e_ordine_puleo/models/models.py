ebf1adc1b18ff17cfad2c4e0420a65960978da52
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-03-16 16:42:58 +0100

    Update models.py

--- abc_preventivo_e_ordine_puleo/models/models.py
+++ abc_preventivo_e_ordine_puleo/models/models.py
@@ -20,15 +20,35 @@ class saleOrder(models.Model):
     @api.onchange("flag_prezzo_configurazione_variante")
     def funzione(self):
         for record in self:
-            if(record.flag_prezzo_configurazione_variante):
-                if(record.order_line):
-                    righe_ordine = record.order_line
-                    _logger.info("Righe ordine %s", righe_ordine)
-                    for riga_ordine in righe_ordine:
-                        descrizione_prodotto_attuale = riga_ordine.name
-                        _logger.info("Descrizione %s", descrizione_prodotto_attuale)
-                        riga_ordine.update({'name': "CIAOOOOOOOOO"})
-                else:
-                    raise ValidationError("Inserire almeno un prodotto.")
-                
+            simbolo_euro = (u"\N{euro sign}")
+            attributi = ""
+            descrizione_prodotto_attuale = ""
             
+            if(not record.order_line):
+                raise ValidationError("Inserire almeno un prodotto.")
+                
+            righe_ordine = record.order_line
+            
+            if(record.flag_prezzo_configurazione_variante):
+                _logger.info("Righe ordine %s", righe_ordine)
+                for riga_ordine in righe_ordine:
+                    if(riga_ordine.product_id.product_template_attribute_value_ids):
+                        descrizione_prodotto_attuale = riga_ordine.name
+                        attributi_prodotto = riga_ordine.product_id.product_template_attribute_value_ids
+                        
+                        for attributo_prodotto in attributi_prodotto:
+                            stringa_attributo = attributo_prodotto.name + " " + simbolo_euro + str(attributo_prodotto.price_extra)
+                            
+                            if(stringa_attributo not in descrizione_prodotto_attuale):
+                                attributi = attributi + stringa_attributo + " \n "
+                                riga_ordine.update({'name': descrizione_prodotto_attuale + attributi})
+                        
+            elif(not record.flag_prezzo_configurazione_variante):
+                for riga_ordine in righe_ordine:
+                    if(not riga_ordine.product_id.product_template_attribute_value_ids):
+                        descrizione_prodotto_attuale = riga_ordine.name
+                        riga_ordine.update({'name': descrizione_prodotto_attuale})
+                    else:
+                        descrizione_prodotto_originaria = riga_ordine.product_template_id.description_sale
+                        riga_ordine.update({'name': descrizione_prodotto_originaria})
+

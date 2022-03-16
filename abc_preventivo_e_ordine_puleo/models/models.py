e09e3aaf7e27018190c1dd45c46bb7ea2df76ac8
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-03-16 15:08:17 +0100

    Update models.py

--- abc_preventivo_e_ordine_puleo/models/models.py
+++ abc_preventivo_e_ordine_puleo/models/models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
+import logging
+_logger = logging.getLogger(__name__)
 
 class abc_preventivo_e_ordinevendita_puleo(models.Model):
     _name = 'abc_preventivo_e_ordine_puleo.abc_preventivo_e_ordine_puleo'
@@ -12,3 +14,21 @@ class abc_preventivo_e_ordinevendita_puleo(models.Model):
 class saleOrder(models.Model):
     _name = "sale.order"
     _inherit = "sale.order"
+    
+    flag_prezzo_configurazione_variante = fields.Boolean(string = "Prezzo configurazione variante", default = False, tracking = True, help = "Flag che attiva la possibilità di vedere i prezzi delle singole configurazioni delle varianti all'interno della descrizione del prodotto.")
+    
+    @api.onchange("flag_prezzo_configurazione_variante")
+    def funzione(self):
+        for record in self:
+            if(record.flag_prezzo_configurazione_variante):
+                if(record.order_line):
+                    righe_ordine = record.order_line
+                    _logger.info("Righe ordine %s", righe_ordine)
+                    for riga_ordine in righe_ordine:
+                        descrizione_prodotto_attuale = riga_ordine.name
+                        _logger.info("Descrizione %s", descrizione_prodotto_attuale)
+                        riga_ordine.update({'name': "CIAOOOOOOOOO"})
+                else:
+                    raise ValidationError("Inserire almeno un prodotto.")
+                
+            

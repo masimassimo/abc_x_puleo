937249236984a06a7127553c8e9d78d9c93a089f
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-03-16 17:16:25 +0100

    Update models.py

--- abc_preventivo_e_ordine_puleo/models/models.py
+++ abc_preventivo_e_ordine_puleo/models/models.py
@@ -23,9 +23,6 @@ class saleOrder(models.Model):
             simbolo_euro = (u"\N{euro sign}")
             attributi = ""
             descrizione_prodotto_attuale = ""
-            
-            if(not record.order_line):
-                raise ValidationError("Inserire almeno un prodotto.")
                 
             righe_ordine = record.order_line
             
@@ -34,21 +31,23 @@ class saleOrder(models.Model):
                 for riga_ordine in righe_ordine:
                     if(riga_ordine.product_id.product_template_attribute_value_ids):
                         descrizione_prodotto_attuale = riga_ordine.name
+                        nome_prodotto = riga_ordine.product_id.name
                         attributi_prodotto = riga_ordine.product_id.product_template_attribute_value_ids
                         
                         for attributo_prodotto in attributi_prodotto:
-                            stringa_attributo = attributo_prodotto.name + " " + simbolo_euro + str(attributo_prodotto.price_extra)
+                            stringa_attributo = "- " + attributo_prodotto.name + " " + simbolo_euro + str(attributo_prodotto.price_extra)
                             
                             if(stringa_attributo not in descrizione_prodotto_attuale):
-                                attributi = attributi + stringa_attributo + " \n "
-                                riga_ordine.update({'name': descrizione_prodotto_attuale + attributi})
+                                attributi = attributi + stringa_attributo + ", \n "
+                                riga_ordine.update({'name': nome_prodotto + ": \n " + attributi + " "})
                         
             elif(not record.flag_prezzo_configurazione_variante):
                 for riga_ordine in righe_ordine:
+                    nome_prodotto = riga_ordine.product_id.display_name
                     if(not riga_ordine.product_id.product_template_attribute_value_ids):
                         descrizione_prodotto_attuale = riga_ordine.name
                         riga_ordine.update({'name': descrizione_prodotto_attuale})
                     else:
                         descrizione_prodotto_originaria = riga_ordine.product_template_id.description_sale
-                        riga_ordine.update({'name': descrizione_prodotto_originaria})
+                        riga_ordine.update({'name': nome_prodotto + descrizione_prodotto_originaria})
 

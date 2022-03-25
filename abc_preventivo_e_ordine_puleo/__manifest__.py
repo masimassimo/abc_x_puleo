d9f230ec60183650b06671bf54e79ff1395b2e21
ABC-Strategie-GitHub <abc.strategie.github@gmail.com>
2022-03-25 15:17:21 +0100

    [ADD]

--- abc_preventivo_e_ordine_puleo/__manifest__.py
+++ abc_preventivo_e_ordine_puleo/__manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://www.abcstrategie.it/",
 
     'category': 'Sale',
-    'version': '0.8',
+    'version': '0.9',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'purchase', 'purchase_order_secondary_unit', 'product_secondary_unit'],

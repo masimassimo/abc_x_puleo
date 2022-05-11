6227eae9f5f4c0791ed233b2fb535aaab8c3e58f
ABC-Strategie-GitHub <96920178+ABC-Strategie-GitHub@users.noreply.github.com>
2022-05-11 12:52:29 +0200

    Update __manifest__.py

--- abc_fattura_imm_e_diff_puleo/__manifest__.py
+++ abc_fattura_imm_e_diff_puleo/__manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://www.abcstrategie.it/",
 
     'category': 'Account',
-    'version': '1.9',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account', 'sale'],
